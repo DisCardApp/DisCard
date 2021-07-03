af0bd6bc2848ce62ddd70b06616525d55d5a328a
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2021-07-03 00:08:45 -0400

    rakuten scrape
    Co-Authored-By: ericj22 <43616400+ericj22@users.noreply.github.com>
    Co-Authored-By: Katherine Li <83562449+k9canine@users.noreply.github.com>
    Co-Authored-By: varGeneric <16168572+vargeneric@users.noreply.github.com>

--- scrape/credit_cards/testjson.py
+++ scrape/credit_cards/testjson.py
@@ -3,4 +3,12 @@ with open('credit_card_info.json', 'r') as File:
     data = File.read()
 
 json_file = json.loads(data)
-print(json_file)
+stores = []
+for key in json_file:
+    if json_file[key]['points']['store_specific']:
+        for store in json_file[key]['points']['store_specific']:
+            if store not in stores:
+                stores.append(store)
+stores.sort()
+for store in stores:
+    print(store)
--- scrape/rakuten/scrape.py
+++ scrape/rakuten/scrape.py
@@ -18,30 +18,35 @@ info = []  # contains sublists of each store with store name, image, cashback am
 for store in soup.find_all(attrs={"class":"store-name"}):
     info.append([store.get_text()])
 
-i = 0
-
 # Store Image Link
+i = 0
 for img in soup.find_all(attrs={"class": "lazy", "height":"27px"}):
     # print(img["data-href"])
     info[i].append(img["data-href"])
     i += 1
 
+# Link
 i = 0
+for link in soup.find_all(attrs={"data-gtm-action": "ShoppingTrip"}):
+    info[i].append(link["data-store-url"])
+    i += 1
 
 # Cashback
+i = 0
 for cashback in soup.find_all(attrs={"class":"now_rebate prox-b f-15 lh-22 cb"}):
     # print(cashback.get_text())
     info[i].append(cashback.get_text().strip())
     i += 1
 
-print(info)
+
 data = {}
 data['stores'] = {}
 for k in range(len(info)):
     data['stores'][info[k][0]] = {}
     data['stores'][info[k][0]]['name'] = info[k][0]
     data['stores'][info[k][0]]['image'] = info[k][1]
-    data['stores'][info[k][0]]['cashback'] = info[k][2]
+    data['stores'][info[k][0]]['link'] = info[k][2]
+    data['stores'][info[k][0]]['cashback'] = info[k][3]
 
 with open('rakuten.json', 'w') as out:
     out.write(json.dumps(data, indent=4))
