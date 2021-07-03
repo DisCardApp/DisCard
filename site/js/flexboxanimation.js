b2014b249e858a3fda3a44dfb01b20e9009f4fb3
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2021-07-02 22:55:01 -0400

    begin ui
    Co-Authored-By: ericj22 <43616400+ericj22@users.noreply.github.com>
    Co-Authored-By: Katherine Li <83562449+k9canine@users.noreply.github.com>
    Co-Authored-By: varGeneric <16168572+vargeneric@users.noreply.github.com>

---
+++ site/js/flexboxanimation.js
@@ -0,0 +1,54 @@
+
+window.addEventListener('load', function(event) {
+  var targetClassName = 'flex-wrap-anim';
+  var defaultDuration = '0.3s';
+
+  var dummyList = [];
+  function addDummy(item, duration) {
+    var top = item.offsetTop;
+    var left = item.offsetLeft;
+    setTimeout(function() {
+      item.style.position = 'absolute';
+      item.style.top = top + 'px';
+      item.style.left = left + 'px';
+
+      var dummyDiv = document.createElement('div');
+      dummyDiv.classList.add(targetClassName + '-dummy');
+      var rect = item.getBoundingClientRect();
+      dummyDiv.style.width = (rect.width + 50) + 'px';
+      dummyDiv.style.height = (rect.height + 50) + 'px';
+      dummyDiv.style.visibility = 'hidden';
+      dummyDiv['__' + targetClassName + '_pair'] = item;
+      dummyDiv['__' + targetClassName + '_duration'] = duration;
+      item.parentNode.appendChild(dummyDiv);
+      dummyList.push(dummyDiv);
+    }, 0);
+  }
+
+  var conts = document.getElementsByClassName(targetClassName);
+  for (var i = 0, max = conts.length; i < max; i++) {
+    var cont = conts[i];
+    cont.style.position = 'relative';
+    var duration = cont.getAttribute('data-duration')
+      || defaultDuration;
+    var items = document.querySelectorAll('.flex-wrap-anim > div');
+    for (var i = 0, max = items.length; i < max; i++) {
+      addDummy(items[i], duration);
+    }
+  }
+
+  window.addEventListener('resize', function(event) {
+    dummyList.forEach(function(dummyDiv) {
+      var item = dummyDiv['__' + targetClassName + '_pair'];
+      var duration = dummyDiv['__' + targetClassName + '_duration'];
+      if (item.offsetTop != dummyDiv.offsetTop) {
+        item.style.transition = 'all ' + duration;
+        item.style.top = dummyDiv.offsetTop + 'px';
+        item.style.left = dummyDiv.offsetLeft + 'px';
+      } else {
+        item.style.transition = '';
+        item.style.left = dummyDiv.offsetLeft + 'px';
+      }
+    });
+  });
+});
---
+++ site/js/loginflow.js
@@ -0,0 +1,54 @@
+var loginBtnHtml = `<img style="width: 20px; padding-top:3px; margin-left:3px; margin-right:3px; float: left;" alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" /><div class="logintext">Sign in with Google</div>`;
+var logoutBtnHtml = `<img style="width: 20px; padding-top:3px; margin-left:3px; margin-right:3px; float: left;" alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" /><span class="signouttext">`;
+
+function signInGoogle() {
+  if (!firebase.auth().currentUser) {
+    let provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope("https://www.googleapis.com/auth/plus.login");
+    firebase.auth().signInWithRedirect(provider);
+  }
+}
+
+function signOut() {
+  return firebase.auth().signOut();
+}
+
+firebase.auth().onAuthStateChanged(function (user) {
+  let googleLoginBtn = document.getElementById("google-login-button");
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    // document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
+    // document.getElementById('quickstart-sign-in').textContent = 'Sign out';
+    // document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+    googleLoginBtn.innerHTML = logoutBtnHtml + "Logged in as " + user.displayName + "</span>";
+    googleLoginBtn.classList.remove("logged-out");
+    googleLoginBtn.classList.add("logged-in");
+    googleLoginBtn.removeEventListener("click", signInGoogle);
+    googleLoginBtn.addEventListener("click", signOut);
+    // var itemCount = firebase.database().ref("users/" + user.uid + "/");
+    // itemCount.on("value", function (snapshot) {
+    //   if (snapshot.val() == null) {
+    //     firebase
+    //       .database()
+    //       .ref("users/" + user.uid +"/itemCount")
+    //       .set({
+    //         itemCount: 0,
+    //       });
+    //   }
+    // });
+  } else {
+    // User is signed out.
+    googleLoginBtn.innerHTML = loginBtnHtml;
+    googleLoginBtn.removeEventListener("click", signOut);
+    googleLoginBtn.addEventListener("click", signInGoogle);
+    googleLoginBtn.classList.remove("logged-in");
+    googleLoginBtn.classList.add("logged-out");
+  }
+});
