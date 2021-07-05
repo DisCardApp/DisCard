9e69f4a742afe0d9843ec372afea5521eb1c26d7
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2021-07-04 23:31:29 -0400

    scheduled scrape
    Co-Authored-By: ericj22 <43616400+ericj22@users.noreply.github.com>
    Co-Authored-By: Katherine Li <83562449+k9canine@users.noreply.github.com>
    Co-Authored-By: varGeneric <16168572+varGeneric@users.noreply.github.com>

--- scrape/rakuten/scrape.py
+++ scrape/rakuten/scrape.py
@@ -27,61 +27,59 @@ import json
 import os
 import requests
 
+def scrape_rakuten():
+    data = []
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36'  # rate limit
+    headers = {'User-Agent': user_agent}
 
-data = []
-user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36'  # rate limit
-headers = {'User-Agent': user_agent}
-skipped = []
-types = []
+    rakuten_link = 'https://www.rakuten.ca/stores'
+    source = requests.get(rakuten_link, headers=headers).text
+    soup = BeautifulSoup(source, "lxml")
 
-rakuten_link = 'https://www.rakuten.ca/stores'
-source = requests.get(rakuten_link, headers=headers).text
-soup = BeautifulSoup(source, "lxml")
+    info = []  # contains sublists of each store with store name, image, cashback amount, respectively
 
-info = []  # contains sublists of each store with store name, image, cashback amount, respectively
+    # Store name
+    for store in soup.find_all(attrs={"class":"store-name"}):
+        info.append([store.get_text()])
 
-# Store name
-for store in soup.find_all(attrs={"class":"store-name"}):
-    info.append([store.get_text()])
+    # Store Image Link
+    i = 0
+    for img in soup.find_all(attrs={"class": "lazy", "height":"27px"}):
+        # print(img["data-href"])
+        info[i].append(img["data-href"])
+        i += 1
 
-# Store Image Link
-i = 0
-for img in soup.find_all(attrs={"class": "lazy", "height":"27px"}):
-    # print(img["data-href"])
-    info[i].append(img["data-href"])
-    i += 1
+    # Link
+    i = 0
+    for link in soup.find_all(attrs={"data-gtm-action": "ShoppingTrip"}):
+        url = f'https://www.rakuten.ca/{link["data-store-url"]}'
+        info[i].append(url)
+        i += 1
 
-# Link
-i = 0
-for link in soup.find_all(attrs={"data-gtm-action": "ShoppingTrip"}):
-    url = f'https://www.rakuten.ca/{link["data-store-url"]}'
-    info[i].append(url)
-    i += 1
+    # Cashback
+    i = 0
+    for cashback in soup.find_all(attrs={"class":"now_rebate prox-b f-15 lh-22 cb"}):
+        # print(cashback.get_text())
+        info[i].append(cashback.get_text().strip())
+        i += 1
 
-# Cashback
-i = 0
-for cashback in soup.find_all(attrs={"class":"now_rebate prox-b f-15 lh-22 cb"}):
-    # print(cashback.get_text())
-    info[i].append(cashback.get_text().strip())
-    i += 1
+    data = {}
+    data['stores'] = {}
+    alphanumeric = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    for k in range(len(info)):
+        name_key = []
+        for char in info[k][0].lower():
+            if char in alphanumeric:
+                name_key.append(char)
+            elif char == " " or char == "-":
+                name_key.append("_")
+        key = "".join(name_key)
+        data['stores'][key] = {}
+        data['stores'][key]['name'] = info[k][0]
+        data['stores'][key]['image'] = info[k][1]
+        data['stores'][key]['link'] = info[k][2]
+        data['stores'][key]['cashback'] = info[k][3]
 
-data = {}
-data['stores'] = {}
-alphanumeric = 'abcdefghijklmnopqrstuvwxyz0123456789'
-for k in range(len(info)):
-    name_key = []
-    for char in info[k][0].lower():
-        if char in alphanumeric:
-            name_key.append(char)
-        elif char == " " or char == "-":
-            name_key.append("_")
-    key = "".join(name_key)
-    data['stores'][key] = {}
-    data['stores'][key]['name'] = info[k][0]
-    data['stores'][key]['image'] = info[k][1]
-    data['stores'][key]['link'] = info[k][2]
-    data['stores'][key]['cashback'] = info[k][3]
-
-with open('../../site/resource/rakuten.json', 'w') as out:
-    out.write(json.dumps(data, indent=4))
+    with open('../../site/resource/rakuten.json', 'w') as out:
+        out.write(json.dumps(data, indent=4))
 
